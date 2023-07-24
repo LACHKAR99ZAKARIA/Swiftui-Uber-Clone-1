4465873b87e039cb712a96535e992ab2d9445aba
LACHKAR99ZAKARIA <lachkar99zaakria@gmail.com>
2023-07-24 10:34:44 +0100

    Initial Commit

---
+++ uberSwiftUIClone 1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  uberSwiftUIClone 1
+//
+//  Created by Zakarai Lachkar on 24/7/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uberSwiftUIClone 1/uberSwiftUIClone_1App.swift
@@ -0,0 +1,17 @@
+//
+//  uberSwiftUIClone_1App.swift
+//  uberSwiftUIClone 1
+//
+//  Created by Zakarai Lachkar on 24/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct uberSwiftUIClone_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
